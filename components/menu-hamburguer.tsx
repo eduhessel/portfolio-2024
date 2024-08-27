b26f9dfdd0aff8631b408c0bdcc35da2775a77d8
Eduardo Hessel <edu.hessel@hotmail.com>
2024-08-27 12:29:05 -0300

    fix: fix into hamburguer menu

--- components/menu-hamburguer.tsx
+++ components/menu-hamburguer.tsx
@@ -22,59 +22,59 @@ export function MenuHamburguer() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
         <DropdownMenuItem>
-            <Link href={"/"} className="w-full">
+            <Link href={"/"} className="w-full h-full">
                 {t("home")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"/sobre"} className="w-full">
+            <Link href={"/sobre"} className="w-full h-full">
                 {t("about")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={""} className="w-full">
+            <Link href={""} className="w-full h-full">
                 {t("6")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={""} className="w-full">
+            <Link href={""} className="w-full h-full">
                 {t("5")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"https://www.behance.net/gallery/201725767/SINV-Bradesco"} className="w-full" target="_blank" rel="noopener noreferrer">
+            <Link href={"https://www.behance.net/gallery/201725767/SINV-Bradesco"} className="w-full h-full" target="_blank" rel="noopener noreferrer">
                 {t("4")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"https://www.behance.net/gallery/199591237/Petinder-UIUX-Design-Case-Study"} className="w-full" target="_blank" rel="noopener noreferrer">
+            <Link href={"https://www.behance.net/gallery/199591237/Petinder-UIUX-Design-Case-Study"} className="w-full h-full" target="_blank" rel="noopener noreferrer">
                 {t("3")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"https://www.behance.net/gallery/176413143/Advisor-App-v20"} className="w-full" target="_blank" rel="noopener noreferrer">
+            <Link href={"https://www.behance.net/gallery/176413143/Advisor-App-v20"} className="w-full h-full" target="_blank" rel="noopener noreferrer">
                 {t("2")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"https://www.behance.net/gallery/118707067/ALLBOOKS-APP-UI-DESIGN"} className="w-full" target="_blank" rel="noopener noreferrer">
+            <Link href={"https://www.behance.net/gallery/118707067/ALLBOOKS-APP-UI-DESIGN"} className="w-full h-full" target="_blank" rel="noopener noreferrer">
                 {t("1")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"https://www.linkedin.com/in/eduardo-hessel"} className="w-full flex gap-2 items-center" target="_blank" rel="noopener noreferrer">
+            <Link href={"https://www.linkedin.com/in/eduardo-hessel"} className="w-full h-full flex gap-2 items-center" target="_blank" rel="noopener noreferrer">
               <LinkedInLogoIcon/>
               {t("linkedin")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"https://github.com/eduhessel"} className="w-full flex gap-2 items-center" target="_blank" rel="noopener noreferrer">
+            <Link href={"https://github.com/eduhessel"} className="w-full h-full flex gap-2 items-center" target="_blank" rel="noopener noreferrer">
               <GitHubLogoIcon/>
               {t("github")}
             </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-            <Link href={"mailto:deveduardohessel@gmail.com"} className="w-full flex gap-2 items-center" target="_blank" rel="noopener noreferrer">
+            <Link href={"mailto:deveduardohessel@gmail.com"} className="w-full h-full flex gap-2 items-center" target="_blank" rel="noopener noreferrer">
               <EnvelopeClosedIcon/>
               {t("e-mail")}
             </Link>
