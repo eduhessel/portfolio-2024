c945b13909c2ebedf13d965d5b46291f370cfe63
Eduardo Hessel <edu.hessel@hotmail.com>
2025-01-10 12:00:23 -0300

    feat: new password richard

--- utils/password-hash.ts
+++ utils/password-hash.ts
@@ -2,4 +2,5 @@ export const correctPasswordHash = [
   "951eebde6581ce2a94877be54f04ebc2f8dd9f0371ef2725c92f2b89dfc12f02", // eduardohessel@2025
   "7c300a898a1868d0643ceda003f77c077f90a7d0eb33326655b7466260558f68", // tfsoftware@2025
   "41d56ee47dbf47bb66afd8cd5a219ef7985a562fc695f812db702b5b168b6e3b", // solutta@2025
+  "78252e344bb0beca7c6200a0281e2297f4ae8349bb285a4d69594dba793d6bba", // richard@2025
 ];
