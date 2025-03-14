8faba8c32208e04fe572dbefed18f0a5b65d33b4
Eduardo Hessel <edu.hessel@hotmail.com>
2025-03-13 23:43:12 -0300

    Update password-hash.ts

--- utils/password-hash.ts
+++ utils/password-hash.ts
@@ -1,4 +1,4 @@
 export const correctPasswordHash = [
   "951eebde6581ce2a94877be54f04ebc2f8dd9f0371ef2725c92f2b89dfc12f02", // eduardohessel@2025
-  "458968dcf6dce34ab386a1156797511f6f4d27535cd9370e4900497e529b3fde", // daniela@2025
+  "e0f9664314d456b49b6adacf07e898400557610e677ff153308d689193d66a9e", // flavio@2025
 ];
